531f0ce9c430658a708d479d54fc1257e204980b
olha.parfeniuk <olha.parfeniuk@b2beings.com>
2025-12-17 15:48:56 +0200

    fix export

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import ProxiesPage from "@/pages/proxies";
 
+// Взагалі тут має бути нормальний роут для сторнінки а не ProxiesPage в Home
 export default function Home() {
   return <ProxiesPage />;
 }
